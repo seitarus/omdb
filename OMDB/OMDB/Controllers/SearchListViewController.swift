b79465778ddec0eb7275477ce52b3effdbc6b60d
Alex <alejandro@jig.es>
2021-10-09 10:42:14 +0200

    Estructura base

---
+++ OMDB/OMDB/Controllers/SearchListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OMDB
+//
+//  Created by iMac 27 iOS on 9/10/21.
+//
+
+import UIKit
+
+class SearchListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
